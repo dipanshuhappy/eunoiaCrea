643db2cbb6e87bb111215f056fba40a2fdf764ba
dipanshuhappy <dipanshuhappy@gmail.com>
2023-02-10 17:30:28 +0530

    first commit

---
+++ participants/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Participant(models.Model):
+    full_name = models.CharField(max_length=255)
+    pen_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    
+    year_and_school = models.TextField(max_length=255)
+    submission_file = models.TimeField(max_length=255)
+    scores = models.BigIntegerField()
+
---
+++ participants/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def leadboard(request):
+    return render(request,"leadboard.html")
